3880f060286bf05e83876c7906bca4c7b626c899
MohiniChauhan09 <134527805+MohiniChauhan09@users.noreply.github.com>
2024-03-28 17:58:44 +0530

    Create leetcode_2958.java

---
+++ leetcode_2958.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int n=nums.length;
+        int i=0; int j=0;
+        int result=0;
+        HashMap<Integer,Integer> mp=new HashMap<>();
+
+        while(j<n){
+            mp.put(nums[j], mp.getOrDefault(nums[j] ,0 ) + 1 );
+
+            while(i<j && mp.getOrDefault(nums[j],0) > k ){
+                mp.put( nums[i] , mp.get(nums[i]) -1 );
+                if(mp.get(nums[i]) == 0 ){
+                    mp.remove(nums[i]);
+                }
+                i++;
+            }
+
+            result= Math.max(result, j-i+1);
+            j++;
+        }
+        return result;
+    }
+}
