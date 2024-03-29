562e1464945e738b6f1844acc9ac9aef54134eee
MohiniChauhan09 <134527805+MohiniChauhan09@users.noreply.github.com>
2024-03-29 23:49:40 +0530

    Create leetcode_2962.java

---
+++ leetcode_2962.java
@@ -0,0 +1,31 @@
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int maxE = Arrays.stream(nums).max().getAsInt();
+
+        int n = nums.length;
+        int i = 0, j = 0;
+
+        long result = 0;
+        int countMax = 0;
+
+        while (j < n) {
+            if (nums[j] == maxE) {
+                countMax++;
+            }
+
+            while (countMax >= k) {
+                result += n - j;
+
+                if (nums[i] == maxE) {
+                    countMax--;
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return result;
+    }
+}
