a62cb77e0e477608207108a6f987e2c232a2ff1c
MohiniChauhan09 <134527805+MohiniChauhan09@users.noreply.github.com>
2024-05-02 10:59:06 +0530

    Create leetcode_2441.java

---
+++ leetcode_2441.java
@@ -0,0 +1,25 @@
+//Approach(Using Sorting + 2-Pointers)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+    public int findMaxK(int[] nums) {
+        Arrays.sort(nums);
+
+        int i = 0;
+        int j = nums.length - 1;
+        
+        while (i < j) {
+            if (-nums[i] == nums[j]) {
+                return nums[j];
+            } 
+            else if (-nums[i] < nums[j]) { 
+                j--;
+            }
+            else { 
+                i++;
+            }
+        }
+        return -1;
+    }
+}
+
