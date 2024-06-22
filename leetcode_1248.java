db7ee8e19e300e339dbf972318e047d087c2202d
MohiniChauhan09 <134527805+MohiniChauhan09@users.noreply.github.com>
2024-06-22 16:08:38 +0530

    Create leetcode_1248.java

---
+++ leetcode_1248.java
@@ -0,0 +1,24 @@
+//Approach(Using prefix sum and storing in hashmap)
+//T.C : O(n)
+//S.C : O(n)
+public class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        int count = 0;
+        int currSum = 0;
+        map.put(currSum, 1);
+
+        for (int i = 0; i < n; i++) {
+            currSum += (nums[i] % 2); // if odd - 1, even - 0
+
+            if (map.containsKey(currSum - k)) {
+                count += map.get(currSum - k);
+            }
+
+            map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
