c66fb273ec9bef9a2af04f3521200a2e90ad8d66
MohiniChauhan09 <134527805+MohiniChauhan09@users.noreply.github.com>
2024-07-07 22:36:48 +0530

    Create leetcode_1518.java

---
+++ leetcode_1518.java
@@ -0,0 +1,18 @@
+//T.C : O(log_numExchange_(numBottles))
+//S.C : O(1)
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int consumed = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            int extraFullBottles = emptyBottles / numExchange;
+            int remain = emptyBottles % numExchange;
+
+            consumed += extraFullBottles;
+            emptyBottles = remain + extraFullBottles;
+        }
+
+        return consumed;
+    }
+}
