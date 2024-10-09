35b84a5c4ca2d7d8f4eb01aa1078c753bc2362c0
MohiniChauhan09 <134527805+MohiniChauhan09@users.noreply.github.com>
2024-10-09 13:42:44 +0530

    Create leetcode_921.java

---
+++ leetcode_921.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open =0; int closed=0;
+        for(char ch : s.toCharArray()){
+            if(ch == '(') open++;
+            else{ //ch == ')'
+                if(open > 0) open-- ;
+                else closed++;
+            }
+        }
+        return open+closed;
+    }
+}
